cf9dfdf99e8d21c32ed84f38a5dd5b4147a81bd8
avienavien <239125961+avienavien@users.noreply.github.com>
2026-09-04 21:25:48 +0400

    FINISHED LINK TRACKER EXTENSION

--- index.js
+++ index.js
@@ -3,35 +3,54 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const saveEl = document.getElementById("save-el")
 const ulEl = document.getElementById("ul-el")
+const deleteEl = document.getElementById("delete-el")
+const saveTabEl = document.getElementById("savetab-el")
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-console.log(leadsFromLocalStorage)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 if(leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
-localStorage.clear()
 
-saveEl.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    
-    renderLeads()
+saveTabEl.addEventListener("click", function(){
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) //save said item to local storage
+    render(myLeads)
     inputEl.focus()
+    })
 })
 
-function renderLeads() {
+
+function render(leads) {
 let listItems = ""
-for(let i = 0; i < myLeads.length; i++){
+for(let i = 0; i < leads.length; i++){
     listItems += ` 
     <li>
-        <a target='_blank' href='${myLeads[i]}'>
-        ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'>
+        ${leads[i]}
         </a>
     </li>
     ` //template string
 }
 ulEl.innerHTML = listItems
 }
+
+deleteEl.addEventListener("dblclick", function() {
+    console.log("")
+    localStorage.clear()
+    myLeads = []
+    inputEl.value = ""
+    render(myLeads)
+})
+saveEl.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
+    render(myLeads)
+    inputEl.focus()
+})
+
+
 
